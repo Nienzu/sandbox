3d9add6e3f7406574032c450d454beedeee12fac
nienzu <ibqqz0602@gmail.com>
2016-06-02 20:24:56 +0800

    aa

--- toc.py
+++ toc.py
@@ -5,9 +5,9 @@ c = 0
 
 pair = sys.argv[1].split(',')
 for line in fileinput.input(sys.argv[2]):
+	print line,
 	for ppp in pair:
 		x=ppp.split('#')
-		if (x[0] in line) && (x[1]in line)
+		if (x[0] in line) and (x[1]in line):
 			c=c+1
-
 print c
