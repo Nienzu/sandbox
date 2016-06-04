e186f08630883a8d4ce1afc50f2dfbb1edd01d3f
nienzu <ibqqz0602@gmail.com>
2016-06-04 09:58:42 +0800

    aa

--- toc.py
+++ toc.py
@@ -1,13 +1,20 @@
 import sys
 import fileinput
 
-c = 0
-
+c=0
+result = []
 pair = sys.argv[1].split(',')
+for ppp in pair:
+	result.append(0)
+	c=c+1
+total_pair=c
 for line in fileinput.input(sys.argv[2]):
-	print line,
+	index=0
 	for ppp in pair:
 		x=ppp.split('#')
 		if (x[0] in line) and (x[1]in line):
-			c=c+1
-print c
+			result[index]=result[index]+1
+		index=index+1
+
+for x in range(total_pair):
+	print result[x]
